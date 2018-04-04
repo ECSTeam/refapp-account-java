5b5a82b8ad73ee6f3b9b6c91fa958d054c88ee73
sulmanraja <sulman.raja@gmail.com>
2018-04-04 16:12:11 -0500

    doing an ls

--- ci/tasks/refapp-account/cf-push/task.sh
+++ ci/tasks/refapp-account/cf-push/task.sh
@@ -2,6 +2,7 @@
 
 set -x
 pwd
+cd refapp-account-java-release
 ls -al
 cf login --skip-ssl-validation -o $CF_ORG -s $CF_SPACE -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
 CMD="cf push -m750m refapp-account-java -p refapp-account-java-0.0.1-SNAPSHOT.jar"
