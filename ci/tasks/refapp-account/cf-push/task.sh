30d0f3201d05a048267664a0b1d2a5fc6b8b1456
sulmanraja <sulman.raja@gmail.com>
2018-04-04 16:28:46 -0500

    added refapp-account-java route

--- ci/tasks/refapp-account/cf-push/task.sh
+++ ci/tasks/refapp-account/cf-push/task.sh
@@ -5,6 +5,6 @@ pwd
 cd refapp-account-java-release
 ls -al
 cf login --skip-ssl-validation -o $CF_ORG -s $CF_SPACE -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
-CMD="cf push -m750m refapp-account-java -p refapp-account-java-0.0.1-SNAPSHOT.jar -route-path=refapp-account-java"
+CMD="cf push -m750m refapp-account-java -p refapp-account-java-0.0.1-SNAPSHOT.jar --route-path refapp-account-java"
 echo $CMD
 $CMD
